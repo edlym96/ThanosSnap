c224449b273c4269461b1f828cb7539af3cf389e
edlym96 <edlym96@hotmail.com>
2019-05-08 19:34:05 +0100

    fixed some formatting

--- app.js
+++ app.js
@@ -6,6 +6,46 @@ var loadFile = function(event) {
 	image.src = URL.createObjectURL(event.target.files[0]);
 };
 
+$("#start-btn").click(function(){
+  
+  html2canvas($(".content")[0]).then(canvas => {
+    //capture all div data as image
+    ctx = canvas.getContext("2d");
+    var imageData = ctx.getImageData(0, 0, canvas.width, canvas.height);
+    var pixelArr = imageData.data;
+    createBlankImageData(imageData);
+    //put pixel info to imageDataArray (Weighted Distributed)
+    for (let i = 0; i < pixelArr.length; i+=4) {
+      //find the highest probability canvas the pixel should be in
+      let p = Math.floor((i/pixelArr.length) *canvasCount);
+      // Make a point to the most probable canvas in the array
+      let a = imageDataArray[weightedRandomDistrib(p)];
+      // Add those pixels into the array
+      a[i] = pixelArr[i];
+      a[i+1] = pixelArr[i+1];
+      a[i+2] = pixelArr[i+2];
+      a[i+3] = pixelArr[i+3]; 
+    }
+    //create canvas for each imageData and append to target element
+    for (let i = 0; i < canvasCount; i++) {
+      let c = newCanvasFromImageData(imageDataArray[i], canvas.width, canvas.height);
+      c.classList.add("dust");
+      $("body").append(c);
+    }
+    //clear all children except the canvas
+    $(".content").children().not(".dust").fadeOut(3500);
+    //apply animation
+    $(".dust").each( function(index){
+      animateBlur($(this),0.8,800);
+      setTimeout(() => {
+        animateTransform($(this),100,-100,chance.integer({ min: -15, max: 15 }),800+(110*index));
+      }, 70*index); 
+      //remove the canvas from DOM tree when faded
+      $(this).delay(70*index).fadeOut((110*index)+800,"easeInQuint",()=> {$( this ).remove();});
+    });
+  });
+})
+
 function weightedRandomDistrib(peak) {
   var prob = [], seq = [];
   for(let i=0;i<canvasCount;i++) {
